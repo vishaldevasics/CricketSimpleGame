9ca1384380cda7c77e4a1df2978c59ed223d1f63
vishaldevasics <vishaldevasics@gmail.com>
2024-03-26 17:37:43 +0530

    Updated code by adding use of functions

--- logic.js
+++ logic.js
@@ -1,2 +1,8 @@
-let randomNum =Math.ceil(3 * Math.random());
-console.log(randomNum);
+let age = 8;
+        let gender = 'Female';
+        let ticketFare = 100;
+        if(age<5) ticketFare = 0;
+        else if(age>65) ticketFare = 30 * ticketFare / 100;
+        else if(gender == 'Female') ticketFare = ticketFare/2;
+        else if(age>=5 && age <=8) ticketFare = 60*ticketFare/100;
+        console.log(ticketFare);
