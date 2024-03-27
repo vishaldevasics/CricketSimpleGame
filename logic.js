3360114052ebf2bbd5da904e68558b20b3bfe51c
vishaldevasics <vishaldevasics@gmail.com>
2024-03-27 17:39:20 +0530

    Score is saved in localStorage in browser and reset button added.

--- logic.js
+++
@@ -1,8 +0,0 @@
-let age = 8;
-        let gender = 'Female';
-        let ticketFare = 100;
-        if(age<5) ticketFare = 0;
-        else if(age>65) ticketFare = 30 * ticketFare / 100;
-        else if(gender == 'Female') ticketFare = ticketFare/2;
-        else if(age>=5 && age <=8) ticketFare = 60*ticketFare/100;
-        console.log(ticketFare);
